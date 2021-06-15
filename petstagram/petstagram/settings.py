054966c35a077f2fe57bf0623b2cedd42344398f
BoyanPeychinov <BoyanPeychinov@gmail.com>
2021-06-15 17:41:10 +0300

    Workshop - part 2 - added some views, templates, urls for new app - pets, it's about listing the objects, view details and hit likes.

--- petstagram/petstagram/settings.py
+++ petstagram/petstagram/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'petstagram.common',
+    'petstagram.pets',
 ]
 
 MIDDLEWARE = [
@@ -78,8 +79,12 @@ WSGI_APPLICATION = 'petstagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pet_db',
+        'USER': 'postgres',
+        'PASSWORD': '20CigariDunhill',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
